cc8a9044278319d36740611f831cfae7b634dd03
Juan Esteban Castaño <juan.castano@ceiba.com.co>
2016-08-09 21:57:20 -0500

    GRAL AJUSTE

--- bullying-web/src/test/java/com/bullying/ut/SecurityTest.java
+++ bullying-web/src/test/java/com/bullying/ut/SecurityTest.java
@@ -1,14 +1,20 @@
 package com.bullying.ut;
 
+import static com.bullying.util.Constants.CONSTANTS;
+import static com.bullying.util.Constants.ERROR_PRUEBA;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
+
+import java.util.ResourceBundle;
 
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.ws.rs.WebApplicationException;
 import javax.ws.rs.core.Response;
 
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
 import org.junit.Test;
 import org.mockito.Mockito;
 
@@ -18,6 +24,8 @@ import com.bullying.service.UserService;
 
 public class SecurityTest 
 {
+	private static final Logger LOGGER = LogManager.getLogger(SecurityTest.class);
+	
 	@Test
 	public void getCookieFacebookFoundTest()
 	{
@@ -79,10 +87,8 @@ public class SecurityTest
 		} 
 		catch (WebApplicationException wae) 
 		{
+			LOGGER.error(ResourceBundle.getBundle(CONSTANTS).getString(ERROR_PRUEBA), wae);
 			assertTrue(wae.getResponse().getStatus() == Response.Status.FORBIDDEN.getStatusCode());
 		}
-			
-		
 	}
-
 }
